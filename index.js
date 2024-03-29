27bc97eccbf2d006be742cca29e2a09235ba66bb
Njagi Githaiga <njagigithaiga@gmail.com>
2024-03-29 14:10:00 +0300

    Submitting_functions_continued_lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // code your solution here
+
+//Instruction 1
+
+function saturdayFun (activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+saturdayFun();
+saturdayFun("bathe my dog");
+console.log(saturdayFun());
+console.log(saturdayFun("bathe my dog"));
+
+
+//Instruction 2
+
+const mondayWork = function (activity='go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+mondayWork();
+mondayWork("work from home");
+console.log(mondayWork());
+console.log(mondayWork("work from home"));
+
+//Instruction 3:
+
+function wrapAdjective (highlight="*") {
+    return function innerF (target="special") {
+        return `You are ${highlight}${target}${highlight}!`
+    }
+}
+
+wrapAdjective("*")("a dedicated programmer");
+console.log(wrapAdjective("*")("a dedicated programmer"));
+
+
